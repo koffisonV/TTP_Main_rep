26b1a97049f803d523032aad3206ddb071457d6d
Koffison V <Koffison29@gmail.com>
2023-07-31 14:06:25 -0400

    React-router-lab added

---
+++ react-router-lab/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import User from './User';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user/:userId" element={<User />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ react-router-lab/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/contact"}>Contact</Link>
+            <Link to={"/user/1"}>User 1</Link>
+            <Link to={"/user/2"}>User 2</Link>
+            <Link to={"/user/3"}>User 3</Link>
+        </nav>
+    );
+}
