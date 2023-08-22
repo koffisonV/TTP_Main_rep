5bb743f4a8939407945da2a830e57a0f0b187873
Koffison V <Koffison29@gmail.com>
2023-08-22 17:54:58 -0400

    Added final touches

--- exploratory-project/src/components/Characters.js
+++ exploratory-project/src/components/Characters.js
@@ -2,6 +2,12 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useCharacters } from "../context/CharactersContext";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Characters() {
   const {
@@ -45,23 +51,36 @@ export default function Characters() {
       <h1 className="text-5xl font-light text-center pt-40 mb-2">
         List of all Characters
       </h1>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {characters.map((character) => (
-          <li key={character.id}>
-            <article className="bg-white p-6 rounded-lg shadow-md">
-              <Link to={`/characters/${character.id}`}>
-                <img
-                  src={character.image}
-                  alt={character.name}
-                  className="w-full h-auto object-cover rounded"
-                />
+      {/* <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={4}
+      modules={[Navigation, Scrollbar, A11y]}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      className=""
+      // onSlideChange={() => console.log('slide change')}
+      // onSwiper={(swiper) => console.log(swiper)}
+    >
+    {characters.map((character) => (
+        <SwiperSlide 
+          className="bg-white p-6 rounded-lg shadow-lg" 
+          key={character.id}
+        >
+          <Link to={`/characters/${character.id}`}>
+          <img
+            src={character.image}
+            alt={character.name}
+            className="w-full h-auto object-cover rounded"
+          />
                 <h2 className="mt-4 text-lg font-semibold">{character.name}</h2>
               </Link>
               <p className="mt-2 text-gray-600">{character.species}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+      {/* </ul> */}
       <div className="flex justify-center mt-6">
         <button
           onClick={handlePrevPage}
--- exploratory-project/src/components/Nav.js
+++ exploratory-project/src/components/Nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="fixed top-0 w-full bg-transparent/10 bg-opacity-30 backdrop-blur-md shadow-2xl bg-gradient-to-b from-white from-70%  via-white via-10%">
+    <div className="absolute top-0 w-full bg-transparent/10 bg-opacity-30 backdrop-blur-md shadow-2xl bg-gradient-to-b from-white from-70%  via-white via-10%">
       <nav className="flex p-3 m-6 space-x-7 text-center font-bold mt-6">
         <div className="py-2 flex-auto duration-300 rounded-md cursor-pointer hover:bg-orange-600">
           <i className="bi bi-house-door-fill pr-1" />
